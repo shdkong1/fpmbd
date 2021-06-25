2d659f6e073af9880adcb989afae1ca95e006750
shidqi <shidqi29@gmail.com>
2021-06-25 18:14:41 +0700

    update external lib

--- src/App.java
+++ src/App.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class App {
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            Connection db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/latihan2", "postgres", "Sh29@Oct23");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
