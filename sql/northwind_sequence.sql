d7cefc4679fbf37bcfa3c50d094f93f47b85f8cc
kelvin2401 <kelvin.andersen5@gmail.com>
2021-06-30 22:35:23 +0700

    Create northwind_sequence.sql

---
+++ sql/northwind_sequence.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION countOrder(date_from date, date_to date)
+	returns int
+	language plpgsql
+as
+$$
+declare
+	order_count integer;
+begin
+	select count(*)
+	into order_count
+	from orders
+	where order_date between date_from and date_to;
+	
+	return order_count;
+end;
+$$;
+
+select countOrder('1996-07-23','1996-10-09');
