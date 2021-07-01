93ed250e9dd05e24c03af9588ecba12f78e74fae
rizft1 <62735317+rizft1@users.noreply.github.com>
2021-07-01 15:18:02 +0700

    Update northwind_procedure.sql

---
+++ sql/northwind_procedure.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE PROCEDURE insert_category (
+		category_id integer, 
+		category_name character varying(15),  
+		description text,
+		picture bytea		
+)
+LANGUAGE plpgsql
+AS $$
+begin
+INSERT INTO categories(category_id,category_name,description,picture) 
+VALUES (category_id,category_name,description,picture) ;
+end;
+$$
